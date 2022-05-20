fd502c3306157012d2fe26ebdf267c35d7f86f79
liming <liming@9xing.com>
2022-05-20 08:38:36 +0800

    add design-patterns

--- docs/8.编程思想/设计模式/index.js
+++ docs/8.编程思想/设计模式/index.js
@@ -1,4 +1,6 @@
-const 
+const creationalDesignPatterns = require('./创建型模式')
+const structuralDesignPatterns = require('./结构型模式')
+const behavioralDesignPatterns = require('./行为型模式')
 
 module.exports = {
   title: '设计模式',
@@ -7,6 +9,8 @@ module.exports = {
       title: '设计模式',
       path: '/8.编程思想/设计模式/',
     },
-
+    creationalDesignPatterns,
+    structuralDesignPatterns,
+    behavioralDesignPatterns,
   ]
 }
