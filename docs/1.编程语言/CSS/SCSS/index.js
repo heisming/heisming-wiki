7c867424fb8ecc6f010aae336d198eb4c0a82679
liming <liming@9xing.com>
2022-05-28 11:05:33 +0800

    updata scss

--- docs/1.编程语言/CSS/SCSS/index.js
+++ docs/1.编程语言/CSS/SCSS/index.js
@@ -1,4 +1,34 @@
 module.exports = {
   title: 'SCSS',
-  path: '/1.编程语言/CSS/SCSS/'
+  path: '/1.编程语言/CSS/SCSS/',
+  children: [
+    {
+      title: 'Sass是什么',
+      path: '/1.编程语言/CSS/SCSS/0.Sass是什么',
+    },
+    {
+      title: 'Sass的基本特性',
+      path: '/1.编程语言/CSS/SCSS/1.Sass的基本特性',
+    },
+    {
+      title: '控制命令',
+      path: '/1.编程语言/CSS/SCSS/2.控制命令',
+    },
+    {
+      title: '字符串函数与数字函数',
+      path: '/1.编程语言/CSS/SCSS/3.字符串函数与数字函数',
+    },
+    {
+      title: 'Map',
+      path: '/1.编程语言/CSS/SCSS/4.Map',
+    },
+    {
+      title: '颜色函数',
+      path: '/1.编程语言/CSS/SCSS/5.颜色函数',
+    },
+    {
+      title: '@规则',
+      path: '/1.编程语言/CSS/SCSS/6.@规则',
+    },
+  ]
 }
