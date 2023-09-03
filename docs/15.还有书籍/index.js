a9a489a816cea1e126689523378c67fcf48fa9ac
heisming <976236190@qq.com>
2023-09-03 17:23:41 +0800

    change something

--- docs/15.还有书籍/index.js
+++ docs/15.还有书籍/index.js
@@ -3,8 +3,8 @@ module.exports = {
   path: '/15.还有书籍/',
   children: [
     {
-      title: '《Vue.js设计与实现》',
-      path: '/15.还有书籍/《Vue.js设计与实现》.md'
+      title: 'Vue.js设计与实现',
+      path: '/15.还有书籍/VueJS设计与实现.md'
     }
   ]
 }
